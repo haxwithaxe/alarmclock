87bab2d636ba824d7130ca7304eeed21c56cf875
haxwithaxe <me@haxwithaxe.net>
2015-03-17 00:43:25 -0400

    added some script code and remembered to initialize the gpio before running.

--- button.py
+++ button.py
@@ -1,16 +1,21 @@
 
 
-import RPi.GPIO as GPIO
+import RPi.GPIO as gpio
 import time
 
 def gpio__init__():
-    GPIO.setmode(GPIO.BCM)
+    gpio.setmode(gpio.BCM)
 
 class Button(object):
 
     def __init__(self, pin):
         self.pin = pin
+	gpio__init__()
         gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_UP)
 
     def poll(self):
         return gpio.input(self.pin) #(gpio.input(self.pin) or False)
+
+if __name__ == "__main__":
+	b = Button(18)
+	print(b.poll())
